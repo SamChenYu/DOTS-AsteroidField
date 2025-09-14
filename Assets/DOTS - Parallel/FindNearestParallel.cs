203ba7f71bd252fd2e7cb6b012664bc47faf8c39
Sam <samchenyu@gmail.com>
2025-09-14 22:45:41 +0100

    DOTS Parallel - converted from IJob to IJobParallelFor

---
+++ Assets/DOTS - Parallel/FindNearestParallel.cs
@@ -0,0 +1,78 @@
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class FindNearestParallel : MonoBehaviour
+{
+    // The size of our arrays does not need to vary, so rather than create
+    // new arrays every field, we'll create the arrays in Awake() and store them
+    // in these fields.
+    NativeArray<float3> TargetPositions;
+    NativeArray<float3> SeekerPositions;
+    NativeArray<float3> NearestTargetPositions;
+
+    public void Start()
+    {
+        Spawner spawner = FindFirstObjectByType<Spawner>();
+        // We use the Persistent allocator because these arrays must
+        // exist for the run of the program.
+        TargetPositions = new NativeArray<float3>(spawner.NumTargets, Allocator.Persistent);
+        SeekerPositions = new NativeArray<float3>(spawner.NumSeekers, Allocator.Persistent);
+        NearestTargetPositions = new NativeArray<float3>(spawner.NumSeekers, Allocator.Persistent);
+    }
+
+    // We are responsible for disposing of our allocations
+    // when we no longer need them.
+    public void OnDestroy()
+    {
+        TargetPositions.Dispose();
+        SeekerPositions.Dispose();
+        NearestTargetPositions.Dispose();
+    }
+
+    public void Update()
+    {
+        // Copy every target transform to a NativeArray.
+        for (int i = 0; i < TargetPositions.Length; i++)
+        {
+            // Vector3 is implicitly converted to float3
+            TargetPositions[i] = Spawner.TargetTransforms[i].localPosition;
+        }
+
+        // Copy every seeker transform to a NativeArray.
+        for (int i = 0; i < SeekerPositions.Length; i++)
+        {
+            // Vector3 is implicitly converted to float3
+            SeekerPositions[i] = Spawner.SeekerTransforms[i].localPosition;
+        }
+
+        // To schedule a job, we first need to create an instance and populate its fields.
+        FindNearestJobParallel findJob = new FindNearestJobParallel
+        {
+            TargetPositions = TargetPositions,
+            SeekerPositions = SeekerPositions,
+            NearestTargetPositions = NearestTargetPositions,
+        };
+
+        // THE ONLY CHANGE IN THIS FILE!!!
+        // This job processes every seeker, so the
+        // seeker array length is used as the index count.
+        // A batch size of 100 is semi-arbitrarily chosen here 
+        // simply because it's not too big but not too small.
+        JobHandle findHandle = findJob.Schedule(SeekerPositions.Length, 100);
+
+
+        // The Complete method will not return until the job represented by
+        // the handle finishes execution. Effectively, the main thread waits
+        // here until the job is done.
+        findHandle.Complete();
+
+        // Draw a debug line from each seeker to its nearest target.
+        for (int i = 0; i < SeekerPositions.Length; i++)
+        {
+            // float3 is implicitly converted to Vector3
+            Debug.DrawLine(SeekerPositions[i], NearestTargetPositions[i], Color.green);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Data Oriented Technology Stack
 
-- Overview of DOTS
+## Overview of DOTS
 
 ECS:
-- Entitiy: Unique ID representing an object in the game
-- Component: pure data / attribute attached to entities
-- System: Logic that processes entities with specific components
+- Entity: Unique ID representing an object in the game
+- Component: Pure data / attribute attached to entities
+- System: Logic that processes entities with specific components  
 (Variables and functions of traditional OOP classes are detached in DOTS)
 
 C# Job System:
@@ -15,10 +15,16 @@ Burst Compiler:
 - Compiles C# jobs into optimized machine code 
 - OOP classes are memory intensvie and not cache friendly. Splitting the data into ECS allows for contiguous memory that allows cache to remain warm.
 
+## Scenes
 
+SingleThread
+- Traditional GameObject based (very slow!!)
 
-
-Asteroids/Dots
+DOTS - 1,000x Seekers & Targets
 - FindNearestDOTS is the MonoBehaviour object that pipelines GameObjects into DOTS/ECS
 - NativeArrays are data structures used for safe parallelization
 - Jobs are scheduled and picked up by the threads for multi-threading
+
+DOTS Parallel - 10,000x Seekers & Targets
+- FindNearestedJobParallel: Converted from IJob to IJobParallelFor
+- Split the SeekerPosition array indicies between threads E.g. thread 1 - [0,49], thread 2 - [50,99], thread 3 - [100,149]
